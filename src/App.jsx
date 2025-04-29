264cb5bba7ec715ccb51cf08f9f564f96bc9615e
Sohila Said <sohilasaid354@gmail.com>
2025-04-29 03:35:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, Navigate, RouterProvider } from 'react-router-dom'
 import Layout from './Layout'
 import Game_category from './Game_category'
 import Game_details from './Game_details'
 
 export default function App() {
 
-  const Routes = createBrowserRouter([
+  const Routes = createHashRouter([
     {
     path:'',
     element:<Layout></Layout>,
@@ -23,6 +23,22 @@ export default function App() {
     
   )
 }
+
+// import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// export default function App() {
+//   return (
+//     <HashRouter>
+//       <Layout>
+//         <Routes>
+//           <Route index element={<Navigate to="category/mmorpg" />} />
+//           <Route path="/category/:category" element={<Game_category />} />
+//           <Route path="/game_details/:id" element={<Game_details />} />
+//         </Routes>
+//       </Layout>
+//     </HashRouter>
+//   );
+// }
 
 
 
