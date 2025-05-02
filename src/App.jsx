60011e2573b80a25ba2a4d9d6b6f4fcd05b211e3
Sohila Said <sohilasaid354@gmail.com>
2025-05-03 02:48:23 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
-import { createBrowserRouter, createHashRouter, Navigate, RouterProvider } from 'react-router-dom'
+import {  HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
 import Game_category from './Game_category'
 import Game_details from './Game_details'
 
 export default function App() {
+  return <HashRouter>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Navigate to="/category/mmorpg" />} />
+      <Route path="category/:category" element={<Game_category />} />
+      <Route path="game_details/:id" element={<Game_details />} />
+    </Route>
+  </Routes>
+</HashRouter>
 
-  const Routes = createHashRouter([
-    {
-    path:'',
-    element:<Layout></Layout>,
-    children:[
-      { index: true, element: <Navigate to="/category/mmorpg" /> },
-      {path:"/category/:category",element:<Game_category/>},
-      {path:"game_details/:id",element:<Game_details/>}
-    ]
-  }
-
-  ])
-  return (
-    <RouterProvider router={Routes}></RouterProvider>
-    
-  )
 }
+
+
+
+  // const Routes = createHashRouter([
+  //   {
+  //   path:'',
+  //   element:<Layout></Layout>,
+  //   children:[
+  //     { index: true, element: <Navigate to="/category/mmorpg" /> },
+  //     {path:"/category/:category",element:<Game_category/>},
+  //     {path:"game_details/:id",element:<Game_details/>}
+  //   ]
+  // }
+
+  // ])
+  // return (
+  //   <RouterProvider router={Routes}></RouterProvider>
+    
+  // )
+
+
+
 
 // import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
